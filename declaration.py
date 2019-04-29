e5b71a6188a177409caa381017250e28eea60876
alina__romanenko <alinaromanenko1@yandex.ru>
2019-04-29 22:53:44 +0700

    git config

--- declaration.py
+++ declaration.py
@@ -6,6 +6,13 @@ class Room:
         self.max_people = max_people
         self.comfort = comfort
 
+    def __str__(self):
+        s = 'номер '+self.number+' '+self.type+' '+self.comfort+' рассчитан на '+self.max_people+' чел'
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Accomodation(Room):
 
@@ -19,12 +26,33 @@ class Hotel:
     def __init__(self, occupation):
         self.occupation = occupation
 
+    def checking(self, number, date): # метод для проверки заполненности номера
+        room = self.occupation[number]
+        if room[date] == 'занято':
+            return 'занято'
+        else:
+            return 'свободно'
+
+    def taken(self, number, date, days): # метод, который занимает нужные номера
+        room = self.occupation[number]
+        for day in range (days+1):
+            room[date+day]='занято'
+
+
+    def __str__(self):
+        return(str(self.occupation))
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Client:
 
-    def __init__(self, name, date, people, days, max_summ):
+    def __init__(self, name, date_in, people, date, days, max_summ):
         self.name = name
-        self.date = date
+        self.date_in = date_in
         self.people = people
+        self.date = date
         self.days = days
         self.max_summ = max_summ
+        self.agreement = True # тут сделать рандом
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from declaration import *
 
-def read_file_rooms():
+def read_file_rooms(): # считывание комнат из файла и формирование элементов класса комнаты
     with open('fund.txt', 'r', encoding='utf8') as f_in:
         rooms = []
         text = f_in.readlines()
@@ -9,10 +9,21 @@ def read_file_rooms():
             element = item.split()
             rooms.append(Room(element[0], element[1], element[2], element[3]))
 
+
     return rooms
 
+def empty_hotel(rooms): # получаем пустой отель, где для всех номеров 31 день свободный
+    empty_days = {}
+    for day in range(1, 32):
+        empty_days[day] = 'свободно'
+    occupation = {}
+    for room in rooms:
+        occupation[room.number] = empty_days
+    return Hotel(occupation)
 
-def variants(rooms):
+
+
+def variants(rooms): # создание словаря со стоимостью БЕЗ ПИТАНИЯ
     options = {}
     type_of_room = {'одноместный': 2900.0, 'двухместный': 2300.0, 'полулюкс': 3200.0, 'люкс': 4100.0}
     type_of_comfort = {'стандарт': 1.0, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
@@ -23,29 +34,82 @@ def variants(rooms):
     return options
 
 
-def food(options):
+def food(options, rooms): # формирование кортежей с номером, питанием, количеством человек и типом питания
     lst = []
-    type_of_food = {'без': 0.0, 'завтрак': 280.0, 'полупансион': 1000.0}
+    type_of_food = {'без питания': 0.0, 'завтрак': 280.0, 'полупансион': 1000.0}
     for key in options.keys():
         for meal in type_of_food.keys():
-            food = (key, options[key]+ type_of_food[meal])
-            lst.append(food)
+            for room in rooms:
+                if room.number == key:
+                    food = (key, options[key]+ type_of_food[meal], int(room.max_people),meal)
+                    lst.append(food)
 
     return lst
 
 
-def sort(for_sort):
-    order = sorted(for_sort, key=lambda x: x[1])
-    print(list(reversed(order)))
+def sort(for_sort): # сортировка кортежей
+    order_ = sorted(for_sort, key=lambda x: x[1])
+    order__ = list(reversed(order_))
+    order = sorted(order__, key=lambda x: x[2])
+    return order
 
-def read_file_booking():
-    pass
-    # считывание данных из файла про гостей
+def read_file_booking(): # считывание данных из файла про гостей
+    with open('booking.txt', 'r', encoding='utf8') as f_in:
+        clients = []
+        text = f_in.readlines()
+        for item in text:
+            item.strip('\n')
+            element = item.split()
+            clients.append(Client(element[0], element[1]+' '+element[2]+' '+element[3], element[4], element[5],
+                                  element[6], element[7]))
+
+    return clients
+
+
+
+
+def hotel_filling(sorted_rooms, clients, hotel, rooms):
+
+    for client in clients:
+        search_people = client.people # количество людей в номер
+        search_summ = client.max_summ # максимальная стоимость
+        search_date = int(client.date.split('.')[0])
+        search_days = int(client.days)
+        room_res = searching(sorted_rooms, search_people, search_summ, search_date, search_days, hotel)
+        if room_res != 0:
+            print ('Найден: \n')
+            for room in rooms:
+                if room.number == room_res[0]:
+                    print(room)
+
+
+
+
+
+
+def searching(sorted_rooms, search_people, search_summ, search_date, search_days, hotel):
+    switch = 0
+    for room in sorted_rooms:
+        if search_people == room[3] and search_summ>room[2]:
+            if hotel.checking(room[0], search_date) == 'занято':
+                continue
+            else:
+                switch = room
+                hotel.taken(room[0], search_date, search_days)
+    return switch
+
 
 
 def main():
-    rooms = read_file_rooms()
-    for_sort = food(variants(rooms))
-    sort(for_sort)
+    rooms = read_file_rooms() # читаем и кладем в список экземпляры комнат
+    hotel = empty_hotel(rooms) # создаем "свободный" отель
+    for_sort = food(variants(rooms), rooms) # формируем кортежи
+    sorted_rooms = sort(for_sort) # сортируем кортежи
+    clients = read_file_booking() # читаем и кладем в список экземпляры клиентов
+
+    hotel_filling(sorted_rooms, clients, hotel, rooms)
+
+    print(sorted_rooms)
+    print(hotel)
 
 main()
